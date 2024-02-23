6004cdd248da1e1e02a28a1bf259222d76fdbf43
Deraybcbc <121023104+AndresOreVel@users.noreply.github.com>
2024-02-23 13:08:10 +0100

    Añadido clase AssetManager

--- juego/core/src/actors/Goku.java
+++ juego/core/src/actors/Goku.java
@@ -1,4 +1,9 @@
 package actors;
 
-public class Goku {
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class Goku extends Actor {
+    public static final int GOKU_STRAIGHT = 0;
+    public static final int GOKU_UP = 1;
+    public static final int GOKU_DOWN = 2;
 }
---
+++ juego/core/src/helpers/AssetManager.java
@@ -0,0 +1,11 @@
+package helpers;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class AssetManager {
+    //Hoja del Sprite
+    public static Texture sheet;
+    public static TextureRegion goku, gokuDown, gokuUp, background;
+
+}
